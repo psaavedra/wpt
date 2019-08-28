c093e312aec77f276d07c7dc8423ca26879824fe
Carlos Alberto Lopez Perez <clopez@igalia.com>
2019-08-28 18:27:16 +0200

    wip: implement tcupload

--- tools/ci/tcdownload.py
+++ tools/ci/tcdownload.py
@@ -1,9 +1,7 @@
 import argparse
 import os
 import logging
-
 import requests
-
 import github
 
 
@@ -15,8 +13,14 @@ def get_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("--ref", action="store", default="master",
                         help="Branch (in the GitHub repository) or commit to fetch logs for")
-    parser.add_argument("--artifact-name", action="store", default="wpt_report.json.gz",
-                        help="Log type to fetch")
+    parser.add_argument("--user", action="store",
+                        help="User name for staging.wpt.fyi", required=True)
+    parser.add_argument("--password", action="store",
+                        help="User password for staging.wpt.fyi", required=True)
+    parser.add_argument("--server-url", action="store",
+                        help="Base server url, defaults to https://staging.wpt.fyi", default="https://staging.wpt.fyi")
+    parser.add_argument("--filter-artifact", action="store",
+                        help="Only get artifacts that contain this string", default=None)
     parser.add_argument("--repo-name", action="store", default="web-platform-tests/wpt",
                         help="GitHub repo name in the format owner/repo. "
                         "This must be the repo from which the TaskCluster run was scheduled "
@@ -36,14 +40,6 @@ def get_json(url, key=None):
         data = data[key]
     return data
 
-
-def get(url, dest, name):
-    resp = requests.get(url)
-    resp.raise_for_status()
-    path = os.path.join(dest, name)
-    with open(path, "w") as f:
-        f.write(resp.content)
-    return path
 
 
 def run(*args, **kwargs):
@@ -73,6 +69,9 @@ def run(*args, **kwargs):
         logger.error("No complete TaskCluster runs found for ref %s" % kwargs["ref"])
         return
 
+    results = []
+    screenshoots = 0
+    wptresults = 0
     for taskgroup in taskgroups:
         taskgroup_url = "https://queue.taskcluster.net/v1/task-group/%s/list"
         artifacts_list_url = "https://queue.taskcluster.net/v1/task/%s/artifacts"
@@ -80,13 +79,40 @@ def run(*args, **kwargs):
         for task in tasks:
             task_id = task["status"]["taskId"]
             url = artifacts_list_url % (task_id,)
+            if kwargs["filter_artifact"] is not None:
+                if kwargs["filter_artifact"] not in task["task"]["metadata"]["name"]:
+                    print("Skipping artifacts for %s" % task["task"]["metadata"]["name"])
+                    continue
+            print("Collecting artifacts for %s" % task["task"]["metadata"]["name"])
             for artifact in get_json(url, "artifacts"):
-                if artifact["name"].endswith(kwargs["artifact_name"]):
-                    filename = "%s-%s-%s" % (task["task"]["metadata"]["name"],
-                                             task_id,
-                                             kwargs["artifact_name"])
-                    path = get("%s/%s" % (url, artifact["name"]), kwargs["out_dir"], filename)
-                    logger.info(path)
+                artifact_url = "%s/%s" % (url, artifact["name"])
+                if artifact["name"].endswith("wpt_report.json.gz"):
+                    results.append(("result_url", artifact_url))
+                    wptresults += 1
+                if artifact["name"].endswith("wpt_screenshot.txt.gz"):
+                    results.append(("screenshot_url", artifact_url))
+                    screenshoots += 1
+
+    if wptresults == 0:
+        print("Not sending data, collected artifacts/wptresults is zero")
+        return 1
+    else:
+        print("Sending a total of %s wptresults and %s screenshoots to %s" % (wptresults, screenshoots, kwargs["server_url"]))
+
+        received = requests.post("%s/api/results/upload" % kwargs["server_url"], data=results,
+                  auth=(kwargs["user"],kwargs["password"]))
+        if received.status_code != 200:
+            print("The server returned an unexpected code")
+            print("Status code is: %s" % received.status_code)
+            print("Content is: %s" % received.content)
+            return 1
+        if "added to queue" not in received.content:
+            print("The server returned an unexpected content")
+            print("Content is: %s" % received.content)
+            return 1
+        print(received.content)
+        print("Check status in: %s/results/?run_id=%s" %(kwargs["server_url"],received.content.split(" ")[1]))
+        return 0
 
 
 def main():
