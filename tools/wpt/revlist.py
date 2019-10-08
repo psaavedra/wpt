5dacee7dd3100446d319e6b4bac23bdb5c853cd3
Pablo Saavedra <psaavedra@igalia.com>
2019-10-08 11:05:35 +0200

    Refactoring of the rev-list wpt subcommad (#19322)
    * Code style is PEP8 conformance.
    
    * Default epoch step set to daily.
    
    * Removed deprecated pretty-format epoch intervals.
    
    * Fix the way of use the `rev-list`command. Now we check the returned
      revision is actually a tagged version.
    
    * Improvements in list_tagged_revisons():
    
      This iterates the rev-list command in bunchs of "rev_list_step" items
    
      skipping the previous "rev_list_offset" and it is incremented in
      "rev_list_step" each time no more valid tagged revisions are found in
      the current set of revisions.
    
      The "rev_list_step" is set to 10000 so even if the execution logic is
      still O(n^2), the n value is so low because each iteration of the
      `rev-list` command returns 10000 items. On the other hand. the memory
      keeps O(n) where n is memory needed for 10000 items.
    
      The loop ends once we reached the required number of revisions to
      return or the are no more tagged revisions returned found by the rev-
      list iterator.
    
      Because it is posible to find the same revision as candidate for two
      consecutives "epoch_step" (case I in the example) the loop checks if
      previous revision added is equal to the current one and skip this on
      positive case.
    
         Fri   Sat   Sun   Mon   Tue   Wed   Thu   Fri   Sat
          |     |     |     |     |     |     |     |     |
       -A---B-C---DEF---G---H--I-----------J-----K-L----M--N--
                                                             ^
                                                            now

--- tools/wpt/revlist.py
+++ tools/wpt/revlist.py
@@ -1,8 +1,9 @@
 import argparse
+import calendar
 import logging
 import os
 import subprocess
-import calendar
+import sys
 import time
 
 here = os.path.dirname(__file__)
@@ -14,19 +15,21 @@ WEEKLY_EPOCH_SIZE = 604800
 WEEKLY_EPOCH_OFFSET = -259200  # Monday, 29 December 1969 0:00:00
 
 DEFAULT_EPOCH_UNTIL = calendar.timegm(time.gmtime())
-DEFAULT_EPOCH_STEP = 3600
+DEFAULT_EPOCH_STEP = 86400
 DEFAULT_NUM_REVISIONS = 1
 DEFAULT_LOG_LEVEL = 30
 DEFAULT_VERBOSE = False
-HEAD_REVISION = "master"
-TAGGED_REVISIONS = "merge_pr_*"
+DEFAULT_HEAD_REVISION = "origin/master"
+
+REV_LIST_COUNT = 10000
+TAGGED_REVISIONS = "merge_pr_"
 
 
 def parser_debug_log_level(**kwargs):
     # type: (**Any) -> [Text|Number]
     debug_level = kwargs.get("log_level", DEFAULT_LOG_LEVEL)
 
-    if isinstance (debug_level, int):
+    if isinstance(debug_level, int):
         return debug_level
 
     try:
@@ -40,18 +43,12 @@ def parser_debug_log_level(**kwargs):
 def parser_epoch_step(**kwargs):
     # type: (**Any) -> Number]
     epoch_step = kwargs.get("epoch_step", DEFAULT_EPOCH_STEP)
-    if isinstance (epoch_step, int):
+    if isinstance(epoch_step, int):
         return epoch_step
-    if epoch_step.lower() == "hourly":
-        return 3600
-    if epoch_step.lower() == "two-hourly":
-        return 7200
-    if epoch_step.lower() == "four-hourly":
-        return 14400
+    if epoch_step.lower() == "three-hourly":
+        return 10800
     if epoch_step.lower() == "six-hourly":
         return 21600
-    if epoch_step.lower() == "eight-hourly":
-        return 28800
     if epoch_step.lower() == "twelve-hourly":
         return 43200
     if epoch_step.lower() == "daily":
@@ -64,7 +61,7 @@ def parser_epoch_step(**kwargs):
 def parser_epoch_until(**kwargs):
     # type: (**Any) -> Number]
     epoch_until = kwargs.get("epoch_until", DEFAULT_EPOCH_UNTIL)
-    if isinstance (epoch_until, int):
+    if isinstance(epoch_until, int):
         return epoch_until
     if epoch_until.lower() == "now":
         return DEFAULT_EPOCH_UNTIL
@@ -75,11 +72,6 @@ def parser_num_revisions(**kwargs):
     # type: (**Any) -> Number]
     num_revisions = kwargs.get("num_revisions", DEFAULT_NUM_REVISIONS)
     return int(num_revisions)
-
-
-def parser_verbose(**kwargs):
-    # type: (**Any) -> Boolean]
-    return kwargs.get("verbose", DEFAULT_VERBOSE)
 
 
 COMMAND_ARGS = {
@@ -95,15 +87,16 @@ COMMAND_ARGS = {
         "name": "epoch-step",
         "default": DEFAULT_EPOCH_STEP,
         "help": "Regular interval of seconds used to get the tagged revision. "
-                "hourly, two-hourly, four-hourly, six-hourly, eight-hourly, "
-                "twelve-hourly, daily, weekly or 3600, 7200, 86000 ...",
+                "three-hourly, six-hourly, eight-hourly, twelve-hourly, "
+                "daily, weekly or 3600, 7200, 10800 ...",
         "parser": parser_epoch_step
     },
-    "num_revisions": {
-        "name": "num-revisions",
-        "default": DEFAULT_NUM_REVISIONS,
-        "help": "Number of revisions to get.",
-        "parser": parser_num_revisions
+    "head_revision": {
+        "name": "head-revision",
+        "default": DEFAULT_HEAD_REVISION,
+        "help": "Head revision (Default: %s)" % DEFAULT_HEAD_REVISION,
+        "parser": lambda **kwargs: kwargs.get("head_revision",
+                                              DEFAULT_HEAD_REVISION),
     },
     "log_level": {
         "name": "log-level",
@@ -112,11 +105,17 @@ COMMAND_ARGS = {
                 "CRITICAL or an integer value like 10, 20 ",
         "parser": parser_debug_log_level
     },
+    "num_revisions": {
+        "name": "num-revisions",
+        "default": DEFAULT_NUM_REVISIONS,
+        "help": "Number of revisions to get.",
+        "parser": parser_num_revisions
+    },
     "verbose": {
         "name": "verbose",
         "default": DEFAULT_VERBOSE,
         "help": "Show more information in the output",
-        "parser": parser_verbose,
+        "parser": lambda **kwargs: kwargs.get("verbose", DEFAULT_VERBOSE),
         "type": bool
     }
 }
@@ -127,10 +126,11 @@ def get_git_cmd(repo_path):
     """Create a function for invoking git commands as a subprocess."""
     def git(cmd, *args):
         # type: (Text, *Union[bytes, Text]) -> Text
-        full_cmd = [u"git", cmd] + list(item.decode("utf8") if isinstance(item, bytes) else item for item in args)  # type: List[Text]
+        arg_list = list(item.decode("utf8") if isinstance(item, bytes) else item for item in args)  # noqa E501
+        full_cmd = [u"git", cmd] + arg_list  # type: List[Text]
         try:
             logger.debug(" ".join(full_cmd))
-            return subprocess.check_output(full_cmd, cwd=repo_path).decode("utf8").strip()
+            return subprocess.check_output(full_cmd, cwd=repo_path).decode("utf8").strip()  # noqa E501
         except subprocess.CalledProcessError as e:
             logger.critical("Git command exited with status %i" % e.returncode)
             logger.critical(e.output)
@@ -138,40 +138,105 @@ def get_git_cmd(repo_path):
     return git
 
 
+def get_tagged_revisions(commit, min_age, skip=0, max_count=10):
+    '''
+    Returns the tagged revisions indexed by the committer date.
+    '''
+    git = get_git_cmd(wpt_root)
+    command = [
+        "--no-walk",
+        "--skip=%s" % skip,
+        "--max-count=%s" % max_count,
+        "--min-age=%s" % min_age,
+        "--format=epoch:%ct:commit:%H:%D",
+        commit
+    ]
+    result = {}
+    for output_line in git("rev-list", *command).split("\n")[1::2]:
+        # print output_line
+        output = output_line.split(":")
+        if len(output) > 5 and output[5].find(TAGGED_REVISIONS) > -1:
+            revision = {}
+            revision["epoch"] = int(output[1])
+            revision["commit"] = output[3].strip()
+            revision["tag"] = output[5].strip()
+            result[int(output[1])] = revision
+    return result
+
+
+def get_newer_tagged_revision(tagged_revisions, min_age):
+    epoch = max(i for i in tagged_revisions.keys() if i < min_age)
+    c = tagged_revisions.keys()
+    c.sort(reverse=True)
+    return tagged_revisions[epoch]
+
+
 def list_tagged_revisons(**kwargs):
     # type: (**Any) -> List[Text]
     logger.debug("list_tagged_revisons: %s" % kwargs)
     git = get_git_cmd(wpt_root)
     epoch_step = COMMAND_ARGS["epoch_step"]["parser"](**kwargs)
+    head_revision = COMMAND_ARGS["head_revision"]["parser"](**kwargs)
     epoch_until = COMMAND_ARGS["epoch_until"]["parser"](**kwargs)
     num_revisions = COMMAND_ARGS["num_revisions"]["parser"](**kwargs)
     verbose = COMMAND_ARGS["verbose"]["parser"](**kwargs)
 
     iterator = 0
     result = []
-    previous_command_output = ""
+    previous_revision = None
+
+    # Iterates the rev-list command in bunchs of "rev_list_step"
+    # items skipping the previous "rev_list_offset" and it is incremented in
+    # "rev_list_step" each time no more valid tagged revisions are found in
+    # the current set of revisions.
+    # The loop ends once we reached the required number of revisions to return
+    # or the are no more tagged revisions returned found by the rev-list
+    # iterator.
+    # Because it is posible to find the same revision as candidate for two
+    # consecutives "epoch_step" (case I in the example) the loop checks if
+    # previous revision added is equal to the current one and skip this on
+    # positive case.
+    #
+    #   Fri   Sat   Sun   Mon   Tue   Wed   Thu   Fri   Sat
+    #    |     |     |     |     |     |     |     |     |
+    # -A---B-C---DEF---G---H--I-----------J-----K-L----M--N--
+    #                                                       ^
+    #                                                      now
+    rev_list_offset = 0
+    rev_list_step = REV_LIST_COUNT
+    tagged_revisions = get_tagged_revisions(head_revision, epoch_until,
+                                            rev_list_offset, rev_list_step)
     while iterator < num_revisions:
-        min_age = (( epoch_until / epoch_step ) - iterator) * epoch_step
+        if len(tagged_revisions) == 0:
+            break
         # For epoch size equal to weekly we need to use a offset to start
         # the week on Monday 00:00:00.
+        epoch_offset = 0
         if epoch_step == WEEKLY_EPOCH_SIZE:
-            min_age += WEEKLY_EPOCH_OFFSET
+            epoch_offset = WEEKLY_EPOCH_OFFSET
+        min_age = ((((epoch_until - epoch_offset) / epoch_step) - iterator) * epoch_step) + epoch_offset  # noqa E501
         if min_age < 0:
             break
-        command = ["--max-count=1", "--tags=%s" % TAGGED_REVISIONS,
-                   "--min-age=%s" % min_age]
-        if verbose:
-            command.append("--format=date: %cI epoch: %ct commit: %H %D")
-        command.append("--no-walk")
-        command.append(HEAD_REVISION)
-        command_output = git("rev-list", *command)
-        if command_output != previous_command_output:
-            result.append(command_output.split("\n")[-1])  # we are only
-            # intereted in the last line of the output
-            previous_command_output = command_output
-        else:
-            num_revisions += 1
-        iterator += 1
+        try:
+            tagged_revision = get_newer_tagged_revision(tagged_revisions,
+                                                        min_age)
+            if previous_revision != tagged_revision["commit"]:
+                if verbose:
+                    result.append("epoch: %(epoch)s commit: %(commit)s tag: %(tag)s" % tagged_revision)  # noqa E501
+                else:
+                    result.append("%(commit)s" % tagged_revision)
+                previous_revision = tagged_revision["commit"]
+            else:
+                num_revisions += 1
+            iterator += 1
+        except ValueError:
+            # Not revisions found in this set of tagged revision older
+            # than epoch_until (skipping the first "rev_list_offset" values)
+            rev_list_offset += rev_list_step
+            tagged_revisions = get_tagged_revisions(head_revision,
+                                                    epoch_until,
+                                                    rev_list_offset,
+                                                    rev_list_step)
     return result
 
 
