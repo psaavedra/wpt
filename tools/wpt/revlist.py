9263870121cc9917ff8ae4f1905e6d549353f193
Pablo Saavedra <psaavedra@igalia.com>
2019-10-10 00:22:54 +0200

    Removed unnecessary arguments for the rev-list subcommand (#19322)
    * Removed epoch-until argument.
    * Removed epoch-threshold argument.
    * Renamed epoch-step to epoch.

--- tools/wpt/revlist.py
+++ tools/wpt/revlist.py
@@ -10,7 +10,7 @@ wpt_root = os.path.abspath(os.path.join(here, os.pardir, os.pardir))
 
 logger = logging.getLogger()
 
-DEFAULT_EPOCH_STEP = 86400
+DEFAULT_EPOCH = 86400
 DEFAULT_EPOCH_THESHOLD = 600
 DEFAULT_EPOCH_UNTIL = calendar.timegm(time.gmtime())
 DEFAULT_MAX_COUNT = 1
@@ -32,32 +32,22 @@ if MYPY:
     from typing import Text
 
 
-def parser_epoch_step(**kwargs):
+def parser_epoch(**kwargs):
     # type: (**Any) -> int
-    epoch_step = kwargs.get("epoch_step", DEFAULT_EPOCH_STEP)
-    if isinstance(epoch_step, int):
-        return epoch_step
-    if epoch_step.lower() == "three-hourly":
+    epoch = kwargs.get("epoch", DEFAULT_EPOCH)
+    if isinstance(epoch, int):
+        return epoch
+    if epoch.lower() == "three-hourly":
         return 10800
-    if epoch_step.lower() == "six-hourly":
+    if epoch.lower() == "six-hourly":
         return 21600
-    if epoch_step.lower() == "twelve-hourly":
+    if epoch.lower() == "twelve-hourly":
         return 43200
-    if epoch_step.lower() == "daily":
+    if epoch.lower() == "daily":
         return 86400
-    if epoch_step.lower() == "weekly":
+    if epoch.lower() == "weekly":
         return 604800
-    return int(epoch_step)
-
-
-def parser_epoch_until(**kwargs):
-    # type: (**Any) -> int
-    epoch_until = kwargs.get("epoch_until", DEFAULT_EPOCH_UNTIL)
-    if isinstance(epoch_until, int):
-        return epoch_until
-    if epoch_until.lower() == "now":
-        return DEFAULT_EPOCH_UNTIL
-    return int(epoch_until)
+    return int(epoch)
 
 
 def parser_max_count(**kwargs):
@@ -67,31 +57,13 @@ def parser_max_count(**kwargs):
 
 
 COMMAND_ARGS = {
-    "epoch_until": {
-        "name": "epoch-until",
-        "default": DEFAULT_EPOCH_UNTIL,
-        "help": "Show revisions older than a specific date in UNIX "
-                "timestamp. NOW is the default value. For example: "
-                "'1570345200'.",
-        "parser": parser_epoch_until
-    },
-    "epoch_threshold": {
-        "name": "epoch-threshold",
-        "default": DEFAULT_EPOCH_THESHOLD,
-        "help": "Safety threshold respect now "
-                "(default: %s seconds)." % DEFAULT_EPOCH_THESHOLD,
-        "parser": lambda **kwargs: int(kwargs.get("epoch_threshold",
-                                                  DEFAULT_EPOCH_THESHOLD)),
-        "type": int
-    },
-
-    "epoch_step": {
-        "name": "epoch-step",
-        "default": DEFAULT_EPOCH_STEP,
+    "epoch": {
+        "name": "epoch",
+        "default": DEFAULT_EPOCH,
         "help": "Regular interval of seconds used to get the tagged revision. "
                 "three-hourly, six-hourly, eight-hourly, twelve-hourly, "
                 "daily, weekly or 3600, 7200, 10800 ...",
-        "parser": parser_epoch_step
+        "parser": parser_epoch
     },
     "max_count": {
         "name": "max-count",
@@ -129,23 +101,23 @@ def get_newer_tagged_revision(tagged_revisions, min_age):
 def list_tagged_revisons(**kwargs):
     # type: (**Any) -> List[Text]
     logger.debug("list_tagged_revisons: %s" % kwargs)
-    epoch_step = COMMAND_ARGS["epoch_step"]["parser"](**kwargs)
-    epoch_threshold = COMMAND_ARGS["epoch_threshold"]["parser"](**kwargs)
-    epoch_until = COMMAND_ARGS["epoch_until"]["parser"](**kwargs)
-    max_count = COMMAND_ARGS["max_count"]["parser"](**kwargs)
-
-    if epoch_until > DEFAULT_EPOCH_UNTIL - epoch_threshold:
-        epoch_until = DEFAULT_EPOCH_UNTIL - epoch_threshold
-        logger.debug("epoch-until adjusted to %s using the safety epoch-threshold value (%s)" % (epoch_until, epoch_threshold))  # noqa E501
-
+    epoch = COMMAND_ARGS["epoch"]["parser"](**kwargs)
+    # For epoch size equal to weekly we need to use a offset to start
+    # the week on Monday 00:00:00.
+    epoch_offset = 0
+    if epoch == WEEKLY_EPOCH_SIZE:
+        epoch_offset = WEEKLY_EPOCH_OFFSET
+    epoch_threshold = DEFAULT_EPOCH_THESHOLD
+    epoch_until = DEFAULT_EPOCH_UNTIL - epoch_threshold
     iterator = 0
+    max_count = COMMAND_ARGS["max_count"]["parser"](**kwargs)
 
     # Iterates the tagged revisions in descending order finding the more
     # recent commit still older than a "min_age" value.
     # When a commit is found "min_age" is set to a new value multiplier of
-    # "epoch_step" but still below of the date of the current commit found.
+    # "epoch" but still below of the date of the current commit found.
     # This needed to deal with intervals where no candidates were found
-    # for the current "epoch_step" and the next candidate found is yet below
+    # for the current "epoch" and the next candidate found is yet below
     # the lower values of the interval (it is the case of J and I for the
     # interval between Wed and Tue, in the example). The algorithm fix
     # the next "min_age" value based on the date value of the current one
@@ -160,12 +132,7 @@ def list_tagged_revisons(**kwargs):
     #                                                      now
     # Expected result: N,M,K,J,H,G,F,C,A
 
-    # For epoch size equal to weekly we need to use a offset to start
-    # the week on Monday 00:00:00.
-    epoch_offset = 0
-    if epoch_step == WEEKLY_EPOCH_SIZE:
-        epoch_offset = WEEKLY_EPOCH_OFFSET
-    min_age = ((((epoch_until - epoch_offset) / epoch_step) - iterator) * epoch_step) + epoch_offset  # noqa E501
+    min_age = ((((epoch_until - epoch_offset) / epoch) - iterator) * epoch) + epoch_offset  # noqa E501
     for tagged_revision in get_tagged_revisions(TAGGED_REVISIONS).split("\n"):
         revision_tag, revision_commit, revision_date, _ = tagged_revision.split(" ")  # noqa E501
         revision_date = int(revision_date)
@@ -176,7 +143,7 @@ def list_tagged_revisons(**kwargs):
         if revision_date <= min_age:
             print(revision_commit)
             iterator += 1
-            min_age = ((((revision_date - epoch_offset) / epoch_step)) * epoch_step) + epoch_offset  # noqa E501
+            min_age = ((((revision_date - epoch_offset) / epoch)) * epoch) + epoch_offset  # noqa E501
 
 
 def get_parser():
@@ -185,10 +152,8 @@ def get_parser():
     for k in sorted(COMMAND_ARGS.iterkeys()):
         command_arg = COMMAND_ARGS[k]
         default_value = command_arg.get("default", None)
-        command_arg_type = command_arg.get("type", None)
         parser.add_argument("--%s" % command_arg["name"],
                             default=default_value,
-                            type=command_arg_type,
                             help=command_arg["help"])
     return parser
 
